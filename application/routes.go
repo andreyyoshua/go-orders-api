2e342faaf01fe3730efb36090b73923a9071e204
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2024-04-27 14:49:55 +0700

    insert handling

--- application/routes.go
+++ application/routes.go
@@ -4,11 +4,12 @@ import (
 	"net/http"
 
 	"github.com/andreyyoshua/go-orders-api/handler"
+	"github.com/andreyyoshua/go-orders-api/repository/order"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func loadRoutes() *chi.Mux {
+func (a *App) loadRoutes() {
 	router := chi.NewRouter()
 
 	router.Use(middleware.Logger)
@@ -16,13 +17,17 @@ func loadRoutes() *chi.Mux {
 	router.Get("/", func (w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
-	router.Route("/orders", loadOrderRoutes)
+	router.Route("/orders", a.loadOrderRoutes)
 
-	return router
+	a.router = router
 }
 
-func loadOrderRoutes(router chi.Router) {
-	orderHandler := &handler.Order{}
+func (a *App)loadOrderRoutes(router chi.Router) {
+	orderHandler := &handler.Order{
+		Repo: &order.RedisRepo {
+			Client: a.rdb,
+		},
+	}
 
 	router.Post("/", orderHandler.Create)
 	router.Get("/", orderHandler.List)
