f95cda60f02db4bffaccf5510b8b54d0a8f44e2b
Andrey Yoshua Manik <andrey.yoshua@gmail.com>
2024-04-27 14:09:15 +0700

    graceful shutdown complete

--- main.go
+++ main.go
@@ -1,28 +1,22 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"net/http"
+	"os"
+	"os/signal"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/andreyyoshua/go-orders-api/application"
 )
 
 func main() {
-	router := chi.NewRouter()
-	router.Use(middleware.Logger)
-	router.Get("/hello", basicHandler)
-	
-	server := &http.Server{
-		Addr: ":3000",
-		Handler: router,
-	}
-	err := server.ListenAndServe()
-	if err != nil {
-		fmt.Println("failed to listen to server", err)
-	}
-}
+	app := application.New()
 
-func basicHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello, world!"))
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+	
+	err := app.Start(ctx)
+	if err != nil {
+		fmt.Println("failed to start app: ", err)
+	}
 }
